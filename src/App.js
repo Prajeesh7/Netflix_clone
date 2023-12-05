5ae21bd73e993ff28310b9cdd88266c3c635ef48
Prajeesh <prajeeshrajan07@gmail.com>
2023-12-05 12:54:00 +0530

    testing axios occuring some error

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/navBar';
 import Banner from './components/Banner/banner';
+import RowPost from './components/RowPosts/rowPost';
 
 function App() {
   return (
     <div className="App">
     <NavBar/>
     <Banner/>
+    <RowPost/>
     </div>
   );
 }
--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './banner.css';
+import { useEffect } from 'react';
+import axios from '../../axios';
+import {API_KEY} from '../../Constants/constants'
 
 function banner() {
+
+  useEffect(() => {
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).than((response)=>{
+      console.log(response.data)
+    })
+  }, [])
   return (
     <div className='banner'>
-        <div className='content'>
-            <h1 className='title'>Movie Name</h1>
-            <div className='banner_buttons'>
-                <button className='button'>Play</button>
-                <button className='button'>My list</button>
-            </div>
-        <p className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the</p>
+      <div className='content'>
+        <h1 className='title'>Movie Name</h1>
+        <div className='banner_buttons'>
+          <button className='button'>Play</button>
+          <button className='button'>My list</button>
         </div>
-    <div className="fade"></div>
+        <p className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the</p>
+      </div>
+      <div className="fade_bottom"></div>
     </div>
   )
 }
