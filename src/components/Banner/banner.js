62b24c0d19ce5bc1e6851f9f6ae31c9185659d2f
Prajeesh <prajeeshrajan07@gmail.com>
2023-12-20 12:10:10 +0530

    Action section edited

--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -11,7 +11,7 @@ function Banner() {
     useEffect(() => {
       axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`).then((response) => {
         //console.log(response.data.results[3])
-        setMovie(response.data.results[Math.floor(Math.random() * response.data.results.length-1)]);
+        setMovie(response.data.results[Math.floor(Math.random() * response.data.results.length-0)]);
       }).catch((err) => {
         alert('Please ensure your internet connection');
       })
